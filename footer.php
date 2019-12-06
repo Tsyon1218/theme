71ea67ea2eaf176386eaae415abae8e4c475e176
Tsyon1218 <57703012+Tsyon1218@users.noreply.github.com>
2019-12-06 08:56:21 -0300

    footer

--- footer.php
+++ footer.php
@@ -1,2 +1,35 @@
+<footer class="footer">
+	<div class="container">
+	<?php if ( is_active_sidebar( 'footer-sidebar' ) ): ?>
+	<div class="widgets-section">
+		<div class="row">
+		   <?php dynamic_sidebar( 'footer-sidebar' );?>   
+		</div>
+	</div>
+	<?php endif; ?>
+	</div>
+</footer>
+	
+	<div class="footer-bottom">
+		<p>
+		<?php
+			if(get_theme_mod('copyright_txt')){
+				echo esc_html(get_theme_mod('copyright_txt'));
+			}else{
+			  esc_html_e('Copyright &copy;','MyPersonalInfo');
+			  echo date_i18n(__('Y','MyPersonalInfo'));
+			  esc_html_e('| MyPersonalInfo','MyPersonalInfo');
+			}
+		?>
+		
+		</p>        
+    </div>
+</div>
+</div>
+
+<div class="scroll-top">
+    <span class="lnr lnr-chevron-up"></span>
+</div>
+<?php wp_footer()?>
 </body>
 </html>
