5352d328c945b19ee11a113e93a276d14d8e1aac
Tsyon1218 <57703012+Tsyon1218@users.noreply.github.com>
2019-12-25 14:08:57 -0300

    Add files via upload

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -2,7 +2,7 @@
 /**
  * Functions which enhance the theme by hooking into WordPress
  *
- * @package mypersonalinfo
+ * @package myPersonalinfo
  */
 
 /**
@@ -11,7 +11,7 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function mypersonalinfo_body_classes( $classes ) {
+function myPersonalinfo_body_classes( $classes ) {
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
@@ -24,14 +24,14 @@ function mypersonalinfo_body_classes( $classes ) {
 
 	return $classes;
 }
-add_filter( 'body_class', 'mypersonalinfo_body_classes' );
+add_filter( 'body_class', 'myPersonalinfo_body_classes' );
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
-function mypersonalinfo_pingback_header() {
+function myPersonalinfo_pingback_header() {
 	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+		echo '<link rel="pingback" href="', esc_url( get_bloginfo( 'pingback_url' ) ), '">';
 	}
 }
-add_action( 'wp_head', 'mypersonalinfo_pingback_header' );
+add_action( 'wp_head', 'myPersonalinfo_pingback_header' );
